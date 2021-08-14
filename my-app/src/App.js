501fa45b01600b68305992c80ec4a1729e93ea62
andreibognar <78687150+andreibognar@users.noreply.github.com>
2021-08-14 18:30:08 +0300

    added comments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+//install bootstrap - package.json
+// npm install bootstrap - doc
+// we will be having this available in all child components
+import bootstrap from 'bootstrap';
 
+// the component itself is a function which is typical for a react component
 function App() {
+  // the function returns the JSX that has to be rendered
   return (
+    // what we see here for now are components that have an equivalent in HTML, but we will proceed in composing our own self-made components
+    // everything must be contained in one node only
     <div className="App">
       <header className="App-header">
+        {/* JSX !== HTML */}
+        {/* attributes in JSX are actually props */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,4 +32,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,22 @@
+// React looks for installed npm packages with the specified name: React, ReactDom
 import React from 'react';
 import ReactDOM from 'react-dom';
+// these are in the src folder - navigation topic
 import './index.css';
+// file extensions are omitted for other modules
+// it imports a default member, {} are missing here
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// this is the actual entry point which calls as follows
+// ReactDOM is an object that was exported from react-dom module and imported in this module
 ReactDOM.render(
+  // this represents the top level component of our app that should be rendered -> JSX syntax which will be translated into React.createElement JS code
   <React.StrictMode>
+    {/* Top level or root component, child node of React.StrictMode */}
     <App />
   </React.StrictMode>,
+  // this is the HTML component where the App component should be rendered in (can be found in index.html file)
   document.getElementById('root')
 );
 
